7f3b8ff18a05c36d26d403a0ab745f7633d64726
JuanEsVal <juanes.valenciag@gmail.com>
2022-11-01 13:33:24 -0500

    add router recipes to App

--- src/recipes/recipes.router.js
+++ src/recipes/recipes.router.js
@@ -1,22 +1,20 @@
 const router = require("express").Router();
-const passport = require("passport");
+const passport = require("passport");                   //Siempre que quiera protejer una ruta (1)
 
 const recipeServices = require("./recipes.services");
-require("../middlewares/auth.middleware")(passport);
+require("../middlewares/auth.middleware")(passport);    //Siempre que quiera protejer una ruta (2)
 
 //? /recipes
 //? /recipes/:recipe_id
 
-router
-  .route("/")
+router.route("/")
   .get(recipeServices.getAllRecipes)
   .post(
-    passport.authenticate("jwt", { session: false }),
+    passport.authenticate("jwt", { session: false }), //Siempre que quiera protejer una ruta (3)
     recipeServices.createRecipe
   );
 
-router
-  .route("/:recipe_id")
+router.route("/:recipe_id")
   .get(recipeServices.getRecipeById)
   .patch(
     passport.authenticate("jwt", { session: false }),
